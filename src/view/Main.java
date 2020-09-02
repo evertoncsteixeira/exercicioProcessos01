084de43cce926e21234f9aaca86a879582386399
everton <everton@everton-VirtualBox>
2020-09-02 12:02:49 -0300

    ExercicioProcessos01

--- src/view/Main.java
+++ src/view/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		RedesController RedeController = new RedesController();
 		System.out.println(RedeController.ip(System.getProperty("os.name")));
-		//System.out.println(RedeController.ping(System.getProperty("os.name")));
+		System.out.println(RedeController.ping(System.getProperty("os.name")));
 	}
 
 }
